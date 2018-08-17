4e3ab576304b5bfa7e694939b45a2fa7c47ab926
liang <shank.0227@gmail.com>
2018-08-18 01:32:21 +0800

    hi there

--- hello world/Program.cs
+++ hello world/Program.cs
@@ -11,6 +11,7 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world.");
+            Console.WriteLine("Hi, there");
         }
     }
 }
